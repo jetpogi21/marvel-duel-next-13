a2ceba1a59ce5c422990a297cb6f985b0a6737d8
jetpogi21 <jet_pradas@yahoo.com>
2023-08-19 16:09:01 +0800

    NextAuth build fix

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
+  /* typescript: {
     // !! WARN !!
     // Dangerously allow production builds to successfully complete even if
     // your project has type errors.
     // !! WARN !!
     ignoreBuildErrors: true,
-  },
+  }, */
   images: {
     domains: ["images.pexels.com"],
   },
