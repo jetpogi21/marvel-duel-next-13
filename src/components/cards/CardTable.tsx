5d5e21d53821f1b3d711b5fcf4f7f9c469c74a31
jetpogi21 <jet_pradas@yahoo.com>
2023-08-27 13:04:25 +0800

    pag row count fix

--- src/components/cards/CardTable.tsx
+++ src/components/cards/CardTable.tsx
@@ -119,7 +119,7 @@ const CardTable: React.FC = () => {
         ]);
       }
 
-      if (dataLastPageRowCount) {
+      if (dataLastPageRowCount !== undefined) {
         setFetchCount(false);
         setRecordCount(dataLastPageRowCount);
       }
